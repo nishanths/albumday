40dfa8ff5bc92d247f14b2103dcbe096d60005b1
Nishanth Shanmugham <nishanths@utexas.edu>
2020-08-23 19:11:54 +0530

    more role attributes

--- web/components/root/index.tsx
+++ web/components/root/index.tsx
@@ -19,40 +19,40 @@ export class RootComponent extends React.Component<RootProps> {
 			<body className="RootBody" />
 		</Helmet>
 
-		return <div className="Root">
+		return <div className="Root" role="main">
 			{helmet}
 			<Link to="/"><div title="[Icon: Birthday by Flatart from thenounproject.com]" className="logo" role="img" alt="App logo"></div></Link>
-			<div className="app-name">
+			<section className="app-name">
 				album birthdays<span className="emph">!</span>
-			</div>
-			<div className="app-desc">
+			</section>
+			<section className="app-desc">
 				Be notified on release date anniversaries for titles in your
 				music library.
-			</div>
-			<div className="get-started">
+			</section>
+			<section className="get-started">
 				<button onClick={() => this.props.history.push(!this.props.loggedIn ? "/start" : "/feed")}>{!this.props.loggedIn ? "Get Started" : "See your feed"}</button>
-			</div>
-			<div className="works-with">
+			</section>
+			<section className="works-with">
 				Works with
-				<div className="services">
-					<span title="Spotify" role="img" alt="Spotify logo" className="service-img spotify"></span>
+				<div className="services" role="list">
+					<span role="listitem" title="Spotify" alt="Spotify logo" className="service-img spotify"></span>
 					<span className="sep">/</span>
-					<span title="Apple Music" role="img" alt="Apple Music logo" className="service-img scrobble"></span>
+					<span role="listitem" title="Apple Music" alt="Apple Music logo" className="service-img scrobble"></span>
 				</div>
-			</div>
-			<div className="faq">
+			</section>
+			<section className="faq">
 				<div className="q">How others are using <span className="emph">{AppName}</span></div>
 				<div className="a">“Rediscover forgotten albums periodically… ”</div>
 				<div className="a">“ …Celebrate by listening to a song on its birthday”</div>
-			</div>
-			<div className="preview">
+			</section>
+			<section className="preview">
 				<a href="/email-preview" target="_blank">Preview sample email</a>
-			</div>
-			<div className="footer-links">
+			</section>
+			<section className="footer-links">
 				<a className="terms" href={"mailto:" + supportEmail}>Support</a>
 				<span className="sep">&nbsp;&nbsp;&middot;&nbsp;&nbsp;</span>
 				<a className="terms" href="/terms">Terms</a>
-			</div>
+			</section>
 		</div>
 	}
 }
